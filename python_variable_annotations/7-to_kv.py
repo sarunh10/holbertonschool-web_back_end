68a831fbed232c223c1463f6f144b6df5ce08176
sarunh10 <sarunh10@gmail.com>
2025-10-10 13:44:47 +0000

    Task 0

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+"""7. Complex types - string and int/float to tuple"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return k, v*v"""
+    return (k, v * v)
