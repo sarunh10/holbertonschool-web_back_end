9a86082a235d1261b958009210d74b441234f82a
sarunh10 <sarunh10@gmail.com>
2025-10-25 20:58:18 +0000

    task11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Python function that returns the list of school
+having a specific topic.
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """ Return a list of schools (documents) that contain the given topic. """
+    result = list(mongo_collection.find({"topics": topic}))
+    return result
